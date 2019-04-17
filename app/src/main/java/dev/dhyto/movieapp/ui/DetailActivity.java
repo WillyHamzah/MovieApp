9677fa5cac11cfd446eca6e4aa526b56e26bc820
Willy Hamzah <wav364@gmail.com>
2019-04-17 21:29:02 +0800

    Kursus

---
+++ app/src/main/java/dev/dhyto/movieapp/ui/DetailActivity.java
@@ -0,0 +1,81 @@
+package dev.dhyto.movieapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import dev.dhyto.movieapp.R;
+import dev.dhyto.movieapp.data.common.Constants;
+import dev.dhyto.movieapp.data.model.MovieResponse;
+import dev.dhyto.movieapp.data.model.TrailerResponse;
+import dev.dhyto.movieapp.data.remote.MovieService;
+import dev.dhyto.movieapp.databinding.ActivityDetailBinding;
+import dev.dhyto.movieapp.ui.adapter.TrailerAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ActivityDetailBinding activityDetailBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MovieResponse.Movie movie = getIntent().getParcelableExtra("movie_intent");
+
+        activityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+
+        displayDetail(movie);
+        initRecyclerView();
+        displayTrailers(movie.getId());
+    }
+
+    void displayDetail(MovieResponse.Movie movie) {
+        activityDetailBinding.detailTop.sectionTitle.movieTitle.setText(movie.getTitle());
+        activityDetailBinding.detailTop.sectionTitle.movieReleaseDate.setText(movie.getReleaseDate());
+
+        activityDetailBinding.detailBottom.synopsis.setText(movie.getOverview());
+
+        Glide.with(this).
+                load(Constants.POSTER_BASE_URL + movie.getPosterPath())
+                .into(activityDetailBinding.detailTop.posterImage);
+
+        Glide.with(this).
+                load(Constants.BACKDROP_BASE_URL + movie.getBackdropPath())
+                .into(activityDetailBinding.detailTop.backropImage);
+    }
+
+    void initRecyclerView() {
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(DetailActivity.this, LinearLayoutManager.HORIZONTAL,
+                        false);
+        activityDetailBinding.detailBottom.rvTrailers.setLayoutManager(layoutManager);
+    }
+
+    void displayTrailers(int movieId) {
+        MovieService.getAPI().getTrailersByMovieId(movieId, "678ef42a1b584848591cbd02ac3899c3").enqueue(new Callback<TrailerResponse>() {
+            @Override
+            public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                if (response.isSuccessful()) {
+                    List<TrailerResponse.ResultsTrailer> trailers = response.body().getResults();
+                    TrailerAdapter adapter = new TrailerAdapter(trailers);
+                    activityDetailBinding.detailBottom.rvTrailers.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                Toast.makeText(DetailActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/dev/dhyto/movieapp/ui/adapter/MovieAdapter.java
@@ -0,0 +1,103 @@
+package dev.dhyto.movieapp.ui.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.palette.graphics.Palette;
+import androidx.recyclerview.widget.RecyclerView;
+import dev.dhyto.movieapp.R;
+import dev.dhyto.movieapp.data.common.Constants;
+import dev.dhyto.movieapp.data.model.MovieResponse;
+import dev.dhyto.movieapp.databinding.ListMovieItemBinding;
+import dev.dhyto.movieapp.ui.DetailActivity;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private List<MovieResponse.Movie> movies;
+    private Context context;
+
+    public MovieAdapter(List<MovieResponse.Movie> movies, Context context) {
+        this.movies = movies;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_movie_item, parent, false);
+
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MovieViewHolder holder, int position) {
+
+        final MovieResponse.Movie movie = movies.get(position);
+        holder.itemView.setOnClickListener(holder);
+        holder.viewDataBinding.setMovie(movie);
+
+//        holder.viewDataBinding.tvMovieTitle.setText(movie.getTitle());
+
+        Glide.with(context)
+                .asBitmap()
+                .load(Constants.POSTER_BASE_URL + movie.getPosterPath())
+                .into(new BitmapImageViewTarget(holder.viewDataBinding.imgMoviePoster) {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        super.onResourceReady(resource, transition);
+
+                        Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                            @Override
+                            public void onGenerated(@Nullable Palette palette) {
+                                holder.viewDataBinding.titleBackground.setBackgroundColor(
+                                        palette.getDarkVibrantColor(context.getResources().getColor(android.R.color.darker_gray)));
+                            }
+                        });
+                    }
+                });
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("movie_intent",movie);
+
+                context.startActivities(new Intent[]{intent});
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ListMovieItemBinding viewDataBinding;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            viewDataBinding = DataBindingUtil.bind(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
